1f492ec7eff98d42281d4a1efa1319a595f879fd
webuilddeveloper <106293550+webuilddeveloper@users.noreply.github.com>
2025-07-01 12:03:17 +0700

    Update CertificateController.cs

--- Controllers/Mobile/CertificateController.cs
+++ Controllers/Mobile/CertificateController.cs
@@ -257,5 +257,54 @@ namespace mobile_api.Controllers
 
         }
 
+        // POST /read
+        [HttpPost("gallery/read")]
+        public ActionResult<Response> GalleryRead([FromBody] Criteria value)
+        {
+            try
+            {
+                var col = new Database().MongoClient<Gallery>("CertificateGallery");
+
+                var filter = Builders<Gallery>.Filter.Eq(x => x.isActive, true);
+                if (!string.IsNullOrEmpty(value.code)) { filter = filter & Builders<Gallery>.Filter.Regex("reference", value.code); }
+                //filter = filter & (Builders<BsonDocument>.Filter.Eq(x => x.B, "4") | Builders<User>.Filter.Eq(x => x.B, "5"));
+
+                var docs = col.Find(filter).Skip(value.skip).Limit(value.limit).Project(c => new { c.imageUrl, c.code }).ToList();
+
+                //var list = new List<object>();
+                //docs.ForEach(doc => { list.Add(BsonSerializer.Deserialize<object>(doc)); });
+                return new Response { status = "S", message = "success", jsonData = docs.ToJson(), objectData = docs };
+            }
+            catch (Exception ex)
+            {
+                return new Response { status = "E", message = ex.Message };
+            }
+        }
+
+        // POST /read
+        [HttpPost("galleryFile/read")]
+        public ActionResult<Response> GalleryFileRead([FromBody] Criteria value)
+        {
+            try
+            {
+                var col = new Database().MongoClient<Gallery>("CertificateGalleryFile");
+
+                var filter = Builders<Gallery>.Filter.Eq(x => x.isActive, true);
+                if (!string.IsNullOrEmpty(value.code)) { filter = filter & Builders<Gallery>.Filter.Regex("reference", value.code); }
+                //filter = filter & (Builders<BsonDocument>.Filter.Eq(x => x.B, "4") | Builders<User>.Filter.Eq(x => x.B, "5"));
+
+                var docs = col.Find(filter).SortBy(o => o.title).Skip(value.skip).Limit(value.limit).Project(c => new { c.imageUrl, c.code, c.type, c.title, c.size }).ToList();
+
+                docs = docs.OrderBy(c => c.title.PadNumbers()).ToList();
+                //var list = new List<object>();
+                //docs.ForEach(doc => { list.Add(BsonSerializer.Deserialize<object>(doc)); });
+                return new Response { status = "S", message = "success", jsonData = docs.ToJson(), objectData = docs };
+            }
+            catch (Exception ex)
+            {
+                return new Response { status = "E", message = ex.Message };
+            }
+        }
+
     }
 }
